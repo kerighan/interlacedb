30e44bcb760a49cb38b8526fe20fdd756f8a007d
Maixent Chenebaux <max.chbx@gmail.com>
2022-01-06 17:31:10 +0100

    test graph

--- test_graph.py
+++ test_graph.py
@@ -1,7 +1,9 @@
-from interlacedb.beta import Graph
-from tqdm import tqdm
-import random
 import os
+import random
+
+from tqdm import tqdm
+
+from interlacedb.beta import Graph
 
 try:
     os.remove("test.db")
@@ -10,20 +12,25 @@ except FileNotFoundError:
 
 G = Graph("test.db")
 
-N = 5000
-M = 50*N
+for i in tqdm(range(1, 1000000)):
+    G.add_edge(0, i)
 
-for i in tqdm(range(N)):
-    G.add_node(i)
+for _ in tqdm(range(1)):
+    G.neighbors(0)
 
-for i in tqdm(range(M)):
-    u = random.randint(0, N-1)
-    v = random.randint(0, N-1)
-    G.add_edge(u, v)
+# N = 5000
+# M = 50*N
 
-for i in tqdm(range(N)):
-    G.neighbors(i)
-    G.predecessors(i)
+# for i in tqdm(range(N)):
+#     G.add_node(i)
 
-print(G.header["n_nodes"])
+# for i in tqdm(range(M)):
+#     u = random.randint(0, N-1)
+#     v = random.randint(0, N-1)
+#     G.add_edge(u, v)
 
+# for i in tqdm(range(N)):
+#     G.neighbors(i)
+#     G.predecessors(i)
+
+# print(G.header["n_nodes"])
